0fe255407449ad83f16c09196a274a0169c8e51c
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-05 03:49:57 -0700

    hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char str[] = "with proper grammar, but the outcome is a piece of art,";
+printf(str);
+return (0);
+}

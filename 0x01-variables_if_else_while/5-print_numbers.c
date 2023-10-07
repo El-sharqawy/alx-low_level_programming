e0760dfafe24f1de2ca8e8694e167a650487db63
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-07 06:12:01 -0700

    C variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints base 10 numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

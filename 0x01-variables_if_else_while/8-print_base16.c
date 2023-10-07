13d25702a17bc1e3b5e05fc18f00164ff9102902
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-07 06:29:27 -0700

    C variables

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints base 16 numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	for (b = 0; b < 10; ++b)
+	{
+		putchar('0' + b);
+	}
+	for (a = 'a'; a <= 'f'; ++a)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}

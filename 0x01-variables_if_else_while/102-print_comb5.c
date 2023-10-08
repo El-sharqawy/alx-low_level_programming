f0d87fe29dd87e5a850a96f23a0b781ea6a07172
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-08 00:38:59 -0700

    C variables advanced

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print all possible combinations of two two-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+	int a, b, c, d;
+
+	for (i = 0; i < 100; i++)
+	{
+		a = i / 10; /* divide on 10  */
+		b = i % 10; /* divide with reminder */
+
+		for (j = 0; j < 100; j++)
+		{
+			c = j / 10; /* divide on 10  */
+			d = j % 10; /* divide with reminder */
+
+			if ((a < c) || ((a == c) && (b < d))
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
+
+				if (!(a == 9 && b == 8))
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
+
+	return (0);
+}

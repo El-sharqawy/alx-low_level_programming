42babe5a0a9e2ef428f763006f646241fb97f444
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-10 04:09:52 -0700

    Nest loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Description: prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}

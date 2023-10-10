4c2c094ffb3845fed4e1f186fdc8e76ae50d6511
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-10 06:49:48 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * times_table - Check Main
+ * @x: an input integer
+ * @y: an input integer
+ * Description: function that adds two integers.
+ * Return: the result
+ */
+int add(int x, int y)
+{
+	return x + y;
+}

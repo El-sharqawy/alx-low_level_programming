c40df7c43678b54dd2dbc6b3545c3254b9f4a49a
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-10 05:47:15 -0700

    nested loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,15 +2,19 @@
 
 /**
  * _islower - checks for lowercase character
+ *
+ * Parameters: int c
+ *
+ * Return: 1 if lowercase, 0 if not lowercase
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 }

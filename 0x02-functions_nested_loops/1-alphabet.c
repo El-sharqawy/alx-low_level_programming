e24370477b071638611cd6d510f58ab1d6fe63d1
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-10 04:43:14 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Description: prints alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alpha(void)
+{
+	char c[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i;
+
+	for (i = 0; i < 27; i++)
+	{
+		_putchar(c[i]);
+	}
+}
+int main(void)
+{
+	print_alpha();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 int _putchar(char c);
+void print_alphabet(void);
 #endif

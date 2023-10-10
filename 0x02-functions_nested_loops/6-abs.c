6bb2bb655a5a4a7e92df8ca9e7c4f4296e682267
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-10 06:04:47 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Check Main
+ * @n: An input integer
+ * Description: function computes the absolute value of an integer.
+ * Return: absolute value.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return -n;
+	else
+		return n;
+}

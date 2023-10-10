4cff35729b7f0d7e90507ae47181d1bb94e34199
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-10 06:19:26 -0700

    nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,12 +10,13 @@ int print_last_digit(int n)
 {
 	if (n < 0)
 	{
-		_putchar('0' + -n % 10);
-		return (-n % 10);
+		unsigned int positive_n = (unsigned int)(-n);
+		_putchar('0' + (positive_n % 10));
+		return (positive_n % 10);
 	}
 	else
 	{
-		_putchar('0' + n % 10);
+		_putchar('0' + (n % 10));
 		return (n % 10);
 	}
 }

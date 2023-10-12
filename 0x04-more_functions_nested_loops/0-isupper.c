5edcd7657f7dc6cdeefaf6b002be2afb73e58454
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-12 10:36:33 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,7 +6,7 @@
  * Description: function returns 1 if the character is uppercase
  * Return: 1 or 0 in otherwise.
  */
-int _isupper(char a) 
+int _isupper(char a)
 {
 	if (a >= 'A' && a <= 'Z')
 		return(1);

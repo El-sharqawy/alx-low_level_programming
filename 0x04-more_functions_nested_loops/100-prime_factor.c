ead25fd19098d0116c1c5a96ea32aa2d4d00a134
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-12 15:33:16 -0700

    more nested loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - print largest prime factor
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int i, f;
+
+	i = 612852475143;
+	for (f = 2; f <= i; f++)
+	{
+		if (i % f == 0)
+		{
+			n /= f;
+			f--;
+		}
+	}
+	printf("%ld\n");
+	return (0);
+}

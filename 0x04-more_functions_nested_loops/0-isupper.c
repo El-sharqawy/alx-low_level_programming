d05f25b0905bb0f2465cdf6b5f82689e3dce33cd
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-12 10:35:59 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 
 /**
  * _isupper - Check Main
- * @c: An input character
+ * @a: An input character
  * Description: function returns 1 if the character is uppercase
  * Return: 1 or 0 in otherwise.
  */

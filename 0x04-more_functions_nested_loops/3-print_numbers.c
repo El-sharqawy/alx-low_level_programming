47ce0f825cd62461d8428cddf9686c2a5d024f62
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-12 11:00:54 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,9 @@
-/*
- * File: 3-print_numbers.c
- * Auth: Osama Elsharqawy
- */
 
 #include "main.h"
 
 /**
  * print_numbers - Check Main
  * Description: function that prints the numbers, from 0 to 9
- * Return: Nothing
  */
 void print_numbers(void)
 {

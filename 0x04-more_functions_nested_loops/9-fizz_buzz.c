18d1679b142e2938192113508dd0b1f7af7e6563
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-12 15:10:12 -0700

    more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,17 +18,27 @@ void fizz_buzz(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf("Fizz ");
 		}
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf("Buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
 		}
 		else
 		{
 			printf("%d ", i);
 		}
 	}
+}
+
+int main(void)
+{
+	fizz_buzz();
+	return(0);
 }

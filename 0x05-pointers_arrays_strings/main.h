eea534357d97457a8cd9e3c9063488dc7667a3c8
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-16 04:30:39 -0700

    pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,10 +2,12 @@
 #define _MAIN_H_
 
 #include <stdio.h>
+#include <string.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+void print_rev(char *s);
 
 #endif /* _MAIN_H_ */

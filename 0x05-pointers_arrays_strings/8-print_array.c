951855488b6715edb90df3e63579538a74e76b0b
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-16 08:07:08 -0700

    pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,16 +10,16 @@
 void print_array(int *a, int n)
 {
 	int i;
-	int x[];
+	int * dynamicIntArray = (int *)malloc(n * sizeof(int));
 
 	for (i = 0; i < n; i++)
 	{
-		x[i] = a[i];
-		printf("%d", x[i]);
+		dynamicIntArray[i] = a[i];
+		printf("%d", dynamicIntArray[i]);
 		if (i != n - 1)
 		{
 			printf(", ");
 		}
 	}
-	printf("\n");
+	free(dynamicIntArray);
 }

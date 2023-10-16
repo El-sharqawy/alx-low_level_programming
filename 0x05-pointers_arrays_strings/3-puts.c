cb4a1468b01bc893651dabef84ed0851adeec3be
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-16 03:49:14 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - prints a string, followed by a new line.
+ * @str: An input character
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	puts(s);
+}

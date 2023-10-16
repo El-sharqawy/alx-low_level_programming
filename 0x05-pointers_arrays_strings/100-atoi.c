46f34103789dc96fd397a217193452da524ed455
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-16 08:50:17 -0700

    pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,5 +7,30 @@
  */
 int _atoi(char *s)
 {
-	return (atoi(s));
+	unsigned int i, count = 0, size = 0, oi = 0, dd = 1, k = 1;
+
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
+		if (*(s + count) == '-')
+			dd *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				k *= 10;
+			size++;
+		}
+
+		count++;
+	}
+
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * k);
+		k /= 10;
+	}
+	return (oi * dd);
 }

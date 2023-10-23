2fb777b5b4b7d9f0d9fde250dbad3b3a1889ff17
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-23 07:09:34 -0700

    more pointers, arrays, strings

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,16 +10,11 @@ void print_diagsums(int *a, int size)
 {
 	int i, mat1 = 0, mat2 = 0;
 
-	for (i = 0; i < (size * size); i++)
+	for (i = 0; i < size; i++)
 	{
-		if (i % (size + 1) == 0)
-		{
-			mat1 += *(a + i);
-		}
-		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
-		{
-			mat2 += *(a + i);
-		}
+		mat1 += a[i * size + i];
+		mat2 += a[i * size + (size - 1 - i)];
 	}
+
 	printf("%d, %d\n", mat1, mat2);
 }

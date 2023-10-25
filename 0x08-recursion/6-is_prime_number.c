5d0a5946b486183eead7bcd1d5f78298e14dd5e5
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-25 08:51:47 -0700

    recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * is_prime_helper - returns 1 if the input integer is a prime number.
+ * @n: an input integer.
+ * @div: an input integer.
+ * Return: 1, otherwise return 0.
+ */
+int is_prime_helper(int n, int div)
+{
+	if (n <= 1)
+		return (0);
+
+	if (div * div > n)
+		return (1);
+
+	if (n % div == 0)
+		return (0);
+
+	return (is_prime_helper(n, div + 1));
+}
+
+/**
+ * is_prime_helper - returns 1 if the input integer is a prime number, otherwise return 0.
+ * @n: an input integer.
+ * Return: 1, otherwise return 0.
+ */
+int is_prime_number(int n)
+{
+	return (is_prime_helper(n, 2));
+}

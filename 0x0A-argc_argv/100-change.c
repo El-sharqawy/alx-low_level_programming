ec1b5a2ef119ee583b3d7349e80f6ac26d1f55b8
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-27 08:26:06 -0700

    C - argc, argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,38 +8,35 @@
  */
 int main(int argc, char *argv[])
 {
+	int i, j, amount = 0, cents = 0;
+	int coins[] = {25, 10, 5, 2, 1};
+	int coin_count[5] = {0};
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	
-	int amount = atoi(argv[1]);
-	
+
+	amount = atoi(argv[1]);
+
 	if (amount < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	
-	int coins[] = {25, 10, 5, 2, 1};
-	int coin_count[5] = {0};
-	int i;
-	
+
 	for (i = 0; i < 5; i++)
 	{
 		coin_count[i] = amount / coins[i];
 		amount = amount % coins[i];
 	}
-	
-	int cents = 0;
-	int j;
-	
+
 	for (j = 0; j < 5; j++)
 	{
 		cents += coin_count[j];
 	}
-	
+
 	printf("%d\n", cents);
 	return (0);
 }

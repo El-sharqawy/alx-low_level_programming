74a4622a0c1a8eea20bf9449fdf702b5f16340dc
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-27 07:12:03 -0700

    C - argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the command-line arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	(void)argv;
+	return (0);
+}

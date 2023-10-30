2178fab7d867ea94ca67542246f6a8d0f867498f
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-30 10:35:57 -0700

    malloc, free

---
+++ 0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars,
+ * and initializes it with a specific char.
+ * @size: size of array.
+ * @c: character to be initialized with.
+ * Return: a pointer to the array, or NULL.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *my_arr;
+	int i;
+
+	if (size <= 0)
+	{
+		return NULL;
+	}
+
+	my_arr = (char*)malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+	{
+		my_arr[i] = c;
+	}
+	return my_arr;
+	free(my_arr);
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c);
+
+#endif /* __MAIN_H__ */

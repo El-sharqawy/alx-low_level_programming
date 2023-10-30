dbacddb89b3f8210870db20b8e840969561118c2
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-30 11:12:32 -0700

    malloc, free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,34 +11,72 @@ char *str_concat(char *s1, char *s2)
 {
 	char *my_str;
 	unsigned int i;
-	unsigned int len1;
-	unsigned int len2;
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 	{
 		return (NULL);
 	}
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-
-	my_str = (char *)malloc(len1 + len2 + 1);
-
-	if (my_str == NULL)
+	if (s1 && s2)
 	{
-		return (NULL);
+		len1 = strlen(s1);
+		len2 = strlen(s2);
+		my_str = (char *)malloc(len1 + len2 + 1);
+
+		if (my_str == NULL)
+		{
+			return (NULL);
+		}
+
+		for (i = 0; i < len1; i++)
+		{
+			my_str[i] = s1[i];
+		}
+
+		for (i = 0; i < len2; i++)
+		{
+			my_str[len1 + i] = s2[i];
+		}
+		my_str[len1 + len2] = '\0';
+	}
+	else if (s1 && s2 == NULL)
+	{
+		len1 = strlen(s1);
+
+		my_str = (char *)malloc(len1 + 1);
+
+		if (my_str == NULL)
+		{
+			return (NULL);
+		}
+
+		for (i = 0; i < len1; i++)
+		{
+			my_str[i] = s1[i];
+		}
+
+		my_str[len1] = '\0';
+	}
+	else if (s1 == NULL && s2)
+	{
+		len2 = strlen(s2);
+
+		my_str = (char *)malloc(len2 + 1);
+
+		if (my_str == NULL)
+		{
+			return (NULL);
+		}
+
+		for (i = 0; i < len2; i++)
+		{
+			my_str[i] = s2[i];
+		}
+
+		my_str[len2] = '\0';
 	}
 
-	for (i = 0; i < len1; i++)
-	{
-		my_str[i] = s1[i];
-	}
-
-	for (i = 0; i < len2; i++)
-	{
-		my_str[len1 + i] = s2[i];
-	}
-
-	my_str[len1 + len2] = '\0';
 	return (my_str);
 }

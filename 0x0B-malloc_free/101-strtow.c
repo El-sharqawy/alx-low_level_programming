a5d0e411e2505e0185e9c14c078e9eba8d4a131e
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-31 04:41:58 -0700

    malloc, free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,10 +1,3 @@
-#include "main.h"
-
-/**
- * strtow - splits a string into words.
- * @str: an input string
- * Return: a pointer to an array of strings, or NULL.
- */
 char **strtow(char *str)
 {
 	int size, i, m, j = 0, k = 0;
@@ -12,13 +5,13 @@ char **strtow(char *str)
 
 	if (str == NULL)
 		return (NULL);
-
 	size = strlen(str);
-	my_words = (char **)malloc((size + 1) * sizeof(char *));
+	if (size == 0)
+		return (NULL)
 
+	my_words = (char **)malloc((size + 1) * sizeof(char *));
 	if (my_words == NULL)
 		return (NULL);
-
 	for (i = 0; i <= size; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
@@ -26,16 +19,13 @@ char **strtow(char *str)
 			if (j > 0)
 			{
 				my_words[k] = (char *)malloc((j + 1) * sizeof(char));
-				
 				if (my_words[k] == NULL)
 					return (NULL);
-
 				for (m = 0; m < j; m++)
 				{
 					my_words[k][m] = str [i - j + m];
 				}
 				my_words[k][j] = '\0';
-
 				k++;
 				j = 0;
 			}
@@ -43,8 +33,11 @@ char **strtow(char *str)
 		else
 			j++;
 	}
-
 	my_words[k] = NULL;
-
+	if (k == 0)
+	{
+		free(my_words);
+		return NULL;
+	}
 	return (my_words);
 }

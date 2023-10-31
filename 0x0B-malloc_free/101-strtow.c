86d92c070364f2ec12b661763ef20e208186de2c
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-10-31 04:51:29 -0700

    malloc, free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,36 +10,27 @@ char **strtow(char *str)
 	int size, i, m, l, j = 0, k = 0;
 	char **my_words;
 
-	if (str == NULL)
+	if (str == NULL || strlen(str) == 0)
 		return (NULL);
 	size = strlen(str);
-	if (size == 0)
-		return (NULL);
-
 	my_words = (char **)malloc((size + 1) * sizeof(char *));
 	if (my_words == NULL)
 		return (NULL);
 	for (i = 0; i <= size; i++)
 	{
-		if (str[i] == ' ' || str[i] == '\0')
+		if (str[i] == ' ' || str[i] == '\0' && j > 0)
 		{
-			if (j > 0)
+			my_words[k] = (char *)malloc((j + 1) * sizeof(char));
+			if (my_words[k] == NULL)
 			{
-				my_words[k] = (char *)malloc((j + 1) * sizeof(char));
-				if (my_words[k] == NULL) {
-					for (l = 0; l < k; l++)
-						free(my_words[l]);
-					free(my_words);
-					return (NULL);
-				}
-				for (m = 0; m < j; m++)
-				{
-					my_words[k][m] = str[i - j + m];
-				}
-				my_words[k][j] = '\0';
-				k++;
-				j = 0;
+				free(my_words);
+				return (NULL);
 			}
+			for (m = 0; m < j; m++)
+				my_words[k][m] = str[i - j + m];
+			my_words[k][j] = '\0';
+			k++;
+			j = 0;
 		}
 		else
 			j++;

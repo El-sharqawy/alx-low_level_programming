910aed7ab79f634a4e70cade27391d196a126824
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-11-02 06:08:23 -0700

    more malloc, free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);

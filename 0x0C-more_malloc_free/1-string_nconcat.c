7b7c940b22c1dbba0b85c145246e56305dfe1221
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-11-02 06:05:36 -0700

    more malloc, free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,33 +12,37 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1 = 0, len2 = n, i;
 	char *mystr;
 
+	if (s1 == NULL && s2 == NULL)
+	{
+		my_str = (char *)malloc(1); /* Allocate memory for an empty string */
+		if (my_str != NULL)
+			my_str[0] = '\0'; /* Make it an empty string */
+		return (my_str);
+	}
+
 	if (s1 == NULL)
-		s1 = "";
+		return (strdup(s2));
 
 	if (s2 == NULL)
-		s1 = "";
+		return (strdup(s1));
 
 	while (s1[len1] != '\0')
 	{
 		len1++;
 	}
-
 	mystr = (char *)malloc(len1 + n + 1);
 	if (mystr == NULL)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; i < len1; i++)
 	{
 		mystr[i] = s1[i];
 	}
-
 	for (i = 0; i < len2; i++)
 	{
 		mystr[len1 + i] = s2[i];
 	}
 	mystr[len1 + len2] = '\0';
-
 	return (mystr);
 }

a313abd092532f028a70f280db7a3cea06deb94b
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-11-06 04:34:26 -0800

    structures & typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * init_dog - initialize variables for struct dog
+ * @d: struct dog
+ * @name: an input name for the dog.
+ * @age: an input age for the dog.
+ * @owner: an input owner name for the dog.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,8 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include "dog.h"
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif

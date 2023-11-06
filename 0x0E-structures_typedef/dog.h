7a52f951e7372ea738f19856b5132fab11609532
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-11-06 04:29:34 -0800

    structures & typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+
+/**
+ * struct dog - a struct that contains dog informations
+ * @name: the name of the dog.
+ * @age: the age of the dog.
+ * @owner: the owner of the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

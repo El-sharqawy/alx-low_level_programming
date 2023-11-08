035cfede817ddc6f53b1407cd19f44f69b5364fc
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-11-08 01:00:42 -0800

    Function Pointers

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef __FUNCTION_POINTERS_H__
+#define __FUNCTION_POINTERS_H__
+
+#include <stdlib.h>
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif /* __FUNCTION_POINTERS_H__

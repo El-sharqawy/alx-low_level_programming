89ae5a98ac71cb2b081f57dcfa6ece431397b31e
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-11-09 04:28:53 -0800

    Variadic Functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(args, char *);
 		if (str == NULL)
-		    str = "(nil)";
+			str = "(nil)";
+
 		if (!separator)
 			printf("%s", str);
 		else if (separator && i == 0)

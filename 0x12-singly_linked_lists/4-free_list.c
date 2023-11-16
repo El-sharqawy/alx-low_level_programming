4caa583ec6ffd70454c79a041d47639d59d4bce6
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-11-16 07:58:48 -0800

    Nodes Project

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,12 +10,13 @@ void free_list(list_t *head)
 	list_t *current;
 	list_t *tmp;
 
+	current = head;
+
 	while (current->next != NULL)
 	{
 		tmp = current;
-		current = current->next;
-		if (tmp->str != NULL)
-			free(tmp->str);
+		free(tmp->str);
 		free(tmp);
+		current = current->next;
 	}
 }

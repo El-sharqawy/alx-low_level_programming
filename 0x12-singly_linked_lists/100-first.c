e03be668337b42900a9c4ef02eeb0971fbedc932
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-11-16 08:08:41 -0800

    Nodes Project

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,8 @@
  * before_main - prints text before main function execution.
  * Return: Nothing.
 */
-void before_main()
+void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }

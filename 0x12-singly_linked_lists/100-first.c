6081e5a302169015092d0f780001a08772794653
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-11-16 08:06:58 -0800

    Nodes Project

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * before_main - prints text before main function execution.
+ * Return: Nothing.
+*/
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

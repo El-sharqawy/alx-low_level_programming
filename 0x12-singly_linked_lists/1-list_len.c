efd26d0279fda9c93323beee29e6c30025d2f1b7
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-11-16 07:07:45 -0800

    Nodes Project

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements in a linked list_t list.
+ * @h: an input list.
+ * Return: the number of nodes.
+*/
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+
+	return (count);
+}

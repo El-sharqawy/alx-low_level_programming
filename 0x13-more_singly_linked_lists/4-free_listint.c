d9188a9dd58436a72960093b6cd2b14117f7f25b
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-11-21 09:22:40 -0800

    More of Linked Lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list.
+ * @head: an input list.
+ * Return: Nothing.
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+	listint_t *tmp;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		tmp = current;
+		free(tmp);
+		current = current->next;
+	}
+}

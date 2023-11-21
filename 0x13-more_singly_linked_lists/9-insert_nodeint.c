1f89863cc8273066eeb557711bcc89d9f65441d7
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-11-21 10:49:14 -0800

    More of Linked Lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *current;
 
 	newList = (listint_t *)malloc(sizeof(listint_t));
-	if (!newList)
+	if (!newList || !head)
 		return (NULL);
 
 	newList->n = n;
@@ -37,7 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!current)
 	{
 		current = *head;
-		while(current->next)
+		while (current->next)
 		{
 			current = current->next;
 		}

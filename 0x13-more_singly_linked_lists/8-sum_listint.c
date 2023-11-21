fe11fec949a0ce193dbff83d3382fd45b8668de3
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-11-21 10:22:23 -0800

    More of Linked Lists

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns the sum of all the data (n)
+ * of a listint_t linked list.
+ * @head: an input list.
+ * Return: the sum of all the data (n), otherwise 0.
+*/
+int sum_listint(listint_t *head)
+{
+	int count = 0;
+
+	if (head == NULL)
+		return 0;
+
+	while (head)
+	{
+		count += (head)->n;
+		(head) = (head)->next;
+	}
+
+	return (count);
+}

644c211c3299d37addf27fdedbc3fe56b5049cc4
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-11-23 02:40:29 -0800

    Bit Manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to unsigned int
+ * @b: and input string of 0 and 1.
+ * Return: converted Number.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int num = 0, base = 1, len = strlen(b), i;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (b[i] == '1')
+			num += base;
+		base *= 2;
+	}
+	return (num);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+unsigned int binary_to_uint(const char *b);
+
+#endif /* __MAIN_H__ */

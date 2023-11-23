0b67e9f05d766e03b4193786590c51f634107fa0
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-11-23 02:58:41 -0800

    Bit Manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,18 +7,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int num = 0, base = 1, len, i;
+	int num = 0, len, i;
 
 	if (!b)
-		return (num);
+		return (0);
 
 	len = strlen(b);
 
-	for (i = len - 1; i >= 0; i--)
+	for (i = 0; i < len; i++)
 	{
-		if (b[i] == '1')
-			num += base;
-		base *= 2;
+		if (b[i] == '1' || b[i] == '0')
+			num = (num << 1) | (b[i] - '0');
+		else
+			return (0);
 	}
 	return (num);
 }

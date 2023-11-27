58b1f4bf68f1e146a1ef4055b5051d5326ccfda0
Osama Elsharqawy <elsharqawy2@gmail.com>
2023-11-27 15:35:43 -0800

    file IO

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,55 +13,23 @@ void ErrorExistFd(int fd)
 
 /**
  * ErrorFiles - exit for files.
- * @f1: file 1/
- * @file1: an input file1.
- * @f2: file 2/
- * @file2: an input file2/
+ * @f1: file 1.
+ * @f2: file 2.
+ * @argv: an input files.
  * Return: Nothing.
  */
-void ErrorFiles(int f1, const char *file1, int f2, const char *file2)
+void ErrorFiles(int f1, int f2, char *argv[])
 {
-	if (f1 == -1 && file1 != NULL)
+	if (f1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file1);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (f2 == -1 && file2 != NULL)
+	if (f2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file2);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-}
-
-/**
- * copy_file - function to copy file to another.
- * @file_from: an input ptr to file to copy from.
- * @file_to: an input ptr to file to copy at.
- * Return: Nothing.
- */
-void copy_file(const char *file_from, const char *file_to)
-{
-	int fd_from, fd_to;
-	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read = BUFFER_SIZE, bytes_written;
-
-	fd_from = open(file_from, O_RDONLY);
-	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	ErrorFiles(fd_from, file_from, fd_to, file_to);
-	while (bytes_read == BUFFER_SIZE)
-	{
-		bytes_read = read(fd_from, buffer, BUFFER_SIZE);
-		if (bytes_read == -1)
-			ErrorFiles(fd_from, file_from, 0, "");
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1)
-			ErrorFiles(0, "", fd_to, file_to);
-
-	}
-	if (close(fd_from) == -1)
-		ErrorExistFd(fd_from);
-	if (close(fd_to) == -1)
-		ErrorExistFd(fd_from);
 }
 
 /**
@@ -74,18 +42,32 @@ void copy_file(const char *file_from, const char *file_to)
  */
 int main(int argc, char **argv)
 {
-	const char *file_from;
-	const char *file_to;
+	int fd_from, fd_to;
+	char buffer[BUFFER_SIZE];
+	ssize_t bytes_read, bytes_written;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
 		exit(97);
 	}
-	file_from = argv[1];
-	file_to = argv[2];
+	fd_from = open(file_from, O_RDONLY);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	ErrorFiles(fd_from, fd_to, argv);
+	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			ErrorFiles(0, bytes_read, argv);
+		}
+	}
+	if (bytes_read == -1)
+		ErrorFiles(bytes_read, 0, argv);
 
-	copy_file(file_from, file_to);
-
+	if (close(fd_from) == -1)
+		ErrorExistFd(fd_from);
+	if (close(fd_to) == -1)
+		ErrorExistFd(fd_to);
 	return (0);
 }

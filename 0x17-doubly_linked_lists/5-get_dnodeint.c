7cc77537388c3b970a05641f430d1c6d0b0b687d
Osama <elsharqawy2@gmail.com>
2024-01-11 21:30:57 +0300

    C double linked lists

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,15 @@
 #include "lists.h"
 
+/**
+* get_dnodeint_at_index - find list at specific index and return it
+*
+* @head: points to a dlistint_t list
+* @index: specific index to search at.
+*
+* Description: a function that finds list at specific index and return it.
+*
+* Return: the node, otherwise NULL.
+*/
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *myList = head;

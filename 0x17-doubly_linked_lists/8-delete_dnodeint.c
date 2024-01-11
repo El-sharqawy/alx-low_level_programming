5dfb9bdd989c0935db3fcd8943c2ace76b8769e6
Osama <elsharqawy2@gmail.com>
2024-01-11 22:38:32 +0300

    C double linked lists

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -14,7 +14,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *tmp;
 	unsigned int curPos = 0;
 
-	if (!head || !*head)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
 	newList = *head;

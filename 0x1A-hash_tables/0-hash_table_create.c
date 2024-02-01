dd94e728be5a9d6550a6eca62c7bacda5a28718f
Osama <elsharqawy2@gmail.com>
2024-02-01 20:23:25 +0300

    Hash Tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,6 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		table->size = size;
 		table->array = (hash_node_t **)malloc(table->size * sizeof(hash_node_t *));
+		/* memset(table->array, 0, table->size * (sizeof(hash_node_t *))); */
 		for (i = 0; i < table->size; i++)
 		{
 			table->array[i] = NULL;

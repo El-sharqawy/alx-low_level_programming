e6383f8c8622a47e1a644336ee0784e843cd598e
Osama <elsharqawy2@gmail.com>
2024-02-02 18:29:21 +0300

    Hash Tables

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,5 +1,12 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_delete - delete and free hash table from the memory.
+ *
+ * @ht: the hash table to be deleted.
+ *
+ * Return: void.
+ */ 
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
